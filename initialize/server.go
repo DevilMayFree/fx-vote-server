c13a32daee0d788d02835a949e96e6a882d7a056
wangjy <1358762936@qq.com>
2024-08-26 20:10:24 +0800

    init

--- initialize/server.go
+++ initialize/server.go
@@ -45,6 +45,25 @@ func isAPIRequest(path string) bool {
 	return false
 }
 
+// cors
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
+}
+
 func runServer() {
 	// init gin server
 
@@ -56,6 +75,9 @@ func runServer() {
 	var users = map[string]string{
 		"admin": "123456", // username: password
 	}
+
+	// 跨域设置
+	routers.Use(Cors())
 
 	// 认证中间件
 	routers.Use(func(c *gin.Context) {
